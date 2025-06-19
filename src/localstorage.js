8f308d38eed10cffc2c4f10233e0bd16fbeb9ea4
leavethetrain <musik201092@web.de>
2025-06-19 21:13:46 +0200

    add jokes to localstorage

---
+++ src/localstorage.js
@@ -0,0 +1,16 @@
+const LOCAL_STORAGE_KEY = "jokes";
+
+export function getSavedJokes() {
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+}
+
+export function saveJoke(joke) {
+  const savedJokes = getSavedJokes();
+
+  if (savedJokes.find((savedJoke) => savedJoke === joke)) {
+    alert("Witz wurde bereits gespeichert");
+    return;
+  }
+  const newSavedJokes = [joke, ...savedJokes];
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newSavedJokes));
+}
